057f4979df7975c6cfe353abd9852d05649f4dfc
Henry Doan <henrydoan96@gmail.com>
2016-05-13 11:23:58 -0600

    person

--- ruby_classes_modules_examples.rb
+++ ruby_classes_modules_examples.rb
@@ -1,20 +1,59 @@
 require 'pry'
-require 'babbler'
+# require 'babbler'
 
-puts 'YAY Friday!'
+# puts 'YAY Friday!'
 
-def keep_coding
-  puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
-  input = gets.strip.downcase # strip and downcase
+# def keep_coding
+#   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
+#   input = gets.strip.downcase # strip and downcase
 
-  if input == 'yes'
-    puts 'Good answer!'
-  else
-    # puts 'You serious, bro? Do you even code?'
-    puts Babbler.babble
-    keep_coding #recurtion
-  end
+#   if input == 'yes'
+#     puts 'Good answer!'
+#   else
+#     # puts 'You serious, bro? Do you even code?'
+#     puts Babbler.babble
+#     keep_coding #recurtion
+#   end
+# end
+
+# keep_coding
+
+class Person
+	def initialize(first_name, last_name, age, gender)
+		@first_name = first_name
+		@last_name = last_name
+		@age = age
+		@gender = gender
+	end
+
+	def first_name
+		puts @first_name
+	end
+
+	def last_name
+		puts @last_name
+	end
+
+	def age
+		puts @age
+	end
+
+	def gender
+		puts @gender
+	end
+
 end
 
-keep_coding
+henry = Person.new('Henry', 'Doan', 19, 'Male')
+henry.first_name
+henry.last_name
+henry.age
+henry.gender
+
+dave = Person.new('Dave', 'Jungst', 35, 'Male')
+dave.first_name
+dave.last_name
+dave.age
+dave.gender
+
 
